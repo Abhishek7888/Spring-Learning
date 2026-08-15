d61ca7f67b4e8b0d9390ab6ffa4c6563cd74d013
Abhishek7888 <abhi13shelke@gmail.com>
2026-08-15 18:29:15 +0530

    Spring setter injection

--- springcore/src/test/java/com/springcore/AppTest.java
+++ springcore/src/test/java/com/springcore/AppTest.java
@@ -1,5 +1,8 @@
 package com.springcore;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
@@ -7,32 +10,34 @@ import junit.framework.TestSuite;
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+public class AppTest extends TestCase {
+	/**
+	 * Create the test case
+	 *
+	 * @param testName name of the test case
+	 */
+	public AppTest(String testName) {
+		super(testName);
+	}
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
+	/**
+	 * @return the suite of tests being tested
+	 */
+	public static Test suite() {
+		return new TestSuite(AppTest.class);
+	}
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+	/**
+	 * Rigourous Test :-)
+		 */
+		public void testApp() {
+		assertTrue(true);
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+		ApplicationContext context = new ClassPathXmlApplicationContext("config.xml");
+		Student student = (Student) context.getBean("student1");
+		System.out.println(student);
+	}
 }
